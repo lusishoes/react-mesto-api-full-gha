ac7f9e1e3e126467295136fe8b7340c4ac230e28
Yury Gavrilov <gavrilov.invest@mail.ru>
2023-09-02 15:24:37 +0800

    15 new

--- frontend/src/components/ProtectedRoute.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { Navigate } from "react-router-dom";
-
-const ProtectedRoute = ({isLoggedIn, element: Component, ...props  }) => {
-  return (
-    isLoggedIn ? <Component {...props} /> : <Navigate to="/signin" replace/>
-)};
-
-export default ProtectedRoute;
